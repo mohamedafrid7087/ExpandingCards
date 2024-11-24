4ad295740ff162d4b0c34e87c7004ec7d5f5f6ec
Mohamed Afrid <meetafrid@gmail.com>
2024-11-24 01:03:36 +0000

    changes made need to check for the repo

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const panels = document.querySelectorAll('.panel')
 panels.forEach( panel => {
-    panel.addEventListener('click', ()=>{
-        removeActiveClasses()
-        panel.classList.add('active') 
+    panel.addEventListener('click', ()=>{   
+        removeActiveClasses()       // removes the active classes first 
+        panel.classList.add('active')  // and adds all the active classes 
     })
 
 })
 
-function removeActiveClasses(){
-    panels.forEach( panel => {
+function removeActiveClasses(){  // remove class 
+    panels.forEach( panel => {    // for each panel removes the active classes 
     panel.classList.remove('active')
 })
 }
